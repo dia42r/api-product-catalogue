97ba7c6f8d535257358f516a1cdd381f73e2cde4
Abdoul DIARRA <abdoul.diarra-prestataire@laposte.fr>
2019-07-01 12:08:03 +0200

    Clean Start spring boot application

---
+++ src/main/java/com/eds/api_catalogue/ApiCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.eds.api_catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCatalogueApplication.class, args);
+	}
+
+}
